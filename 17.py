61afcc885ecca84cd81d05867472ebcf3fa775a5
Hugh Williams <hughaw64@gmail.com>
2018-02-03 17:22:03 +0000

    Heavily optimized code and partway through improving safety of language

--- 17.py
+++ 17.py
@@ -1,10 +1,11 @@
 import argparse
 import re
 
+import to_javascript
 import to_python
 
 FUNCTIONS = '([0-9a-g]+) ?{([^}]*)}'
-MAX = 18446744073709551615
+MAX = 18446744073709551616
 
 def parse(code):
     ast = {}
@@ -13,26 +14,48 @@ def parse(code):
             print('Duplicate function:', name)
             exit()
         name = int(name, 17)
-        print(name)
         ops = []
+        stack_min = 0
         for op in re.findall('([\S]+)', data):
-            print(op)
             if re.fullmatch('[0-9a-g]+', op):
-                ops.append(('INT', int(op, 17) % MAX))
+                ops.append(('INT', int(op, 17) % MAX, stack_min))
+                stack_min += 1
             elif re.fullmatch(r'\+', op):
-                ops.append(('ADD', op))
+                ops.append(('ADD', op, stack_min))
+                stack_min = max(1, stack_min - 1)
             elif re.fullmatch(r'\-', op):
-                ops.append(('SUB', op))
+                ops.append(('SUB', op, stack_min))
+                stack_min = max(1, stack_min - 1)
             elif re.fullmatch(r'\*', op):
-                ops.append(('MUL', op))
-            elif re.fullmatch(r'\\', op):
-                ops.append(('DIV', op))
+                ops.append(('MUL', op, stack_min))
+                stack_min = max(1, stack_min - 1)
+            elif re.fullmatch(r'\/', op):
+                ops.append(('DIV', op, stack_min))
+                stack_min = max(1, stack_min - 1)
             elif re.fullmatch(r'\@', op):
-                ops.append(('STORE', op))
+                ops.append(('STORE', op, stack_min))
+                stack_min = max(0, stack_min - 2)
             elif re.fullmatch(r'\#', op):
-                ops.append(('LOAD', op))
+                ops.append(('LOAD', op, stack_min))
+                stack_min = max(1, stack_min)
+            elif re.fullmatch(r':', op):
+                ops.append(('DUP', op, stack_min))
+                stack_min += 1
+            elif re.fullmatch(r'==', op):
+                ops.append(('EQ', op, stack_min))
+                stack_min = max(1, stack_min - 1)
+            elif re.fullmatch(r'!=', op):
+                ops.append(('NTEQ', op, stack_min))
+                stack_min = max(1, stack_min - 1)
+            elif re.fullmatch(r'>', op):
+                ops.append(('GREATER', op, stack_min))
+                stack_min = max(1, stack_min - 1)
+            elif re.fullmatch(r'<', op):
+                ops.append(('LESS', op, stack_min))
+                stack_min = max(1, stack_min - 1)
             elif re.fullmatch(r'\$', op):
-                ops.append(('OUTPUT', op))
+                ops.append(('OUTPUT', op, stack_min))
+                stack_min = max(0, stack_min - 1)
             else:
                 print('Unknown OP:', op)
         if ops:
@@ -47,31 +70,57 @@ def optimize(ast):
             block = ast[name]
             for i, x in enumerate(block):
                 try:
-                    if x[0] == 'INT' and ast[name][i + 1][0] == 'INT':
+                    if x[0] == 'INT' and ast[name][i + 1][0] != 'INT':
+                        if block[i + 1][0] == 'ADD':
+                            block[i:i + 2] = [('ADD', x[1], block[i][2])]
+                            changed = True
+                        elif block[i + 1][0] == 'SUB':
+                            block[i:i + 2] = [('SUB', x[1], block[i][2])]
+                            changed = True
+                        elif block[i + 1][0] == 'MUL':
+                            block[i:i + 2] = [('MUL', x[1], block[i][2])]
+                            changed = True
+                        elif block[i + 1][0] == 'DIV':
+                            block[i:i + 2] = [('DIV', x[1], block[i][2])]
+                            changed = True
+                        elif block[i + 1][0] == 'DUP':
+                            block[i:i + 2] = [('DUP', x[1], block[i][2])]
+                            changed = True
+                        elif block[i + 1][0] == 'STORE':
+                            block[i:i + 2] = [('STORE', x[1], block[i][2])]
+                            changed = True
+                        elif block[i + 1][0] == 'LOAD':
+                            block[i:i + 2] = [('LOAD', x[1], block[i][2])]
+                            changed = True
+                        elif block[i + 1][0] == 'OUTPUT':
+                            block[i:i + 2] = [('OUTPUT', x[1], block[i][2])]
+                            changed = True
+                    elif x[0] == 'INT' and ast[name][i + 1][0] == 'INT':
                         if block[i + 2][0] == 'ADD':
                             num = (block[i][1] + block[i + 1][1]) % MAX
-                            block[i:i + 3] = [('INT', num)]
+                            block[i:i + 3] = [('INT', num, block[i + 1][2])]
                             changed = True
                         elif block[i + 2][0] == 'SUB':
                             num = (block[i][1] - block[i + 1][1]) % MAX
-                            print(block[i], block[i+1])
-                            block[i:i + 3] = [('INT', num)]
+                            block[i:i + 3] = [('INT', num, block[i + 1][2])]
                             changed = True
                         elif block[i + 2][0] == 'MUL':
                             num = (block[i][1] * block[i + 1][1]) % MAX
-                            block[i:i + 3] = [('INT', num)]
+                            block[i:i + 3] = [('INT', num, block[i + 1][2])]
                             changed = True
                         elif block[i + 2][0] == 'DIV':
-                            num = int(block[i][1] / block[i + 1][1]) % MAX
-                            block[i:i + 3] = [('INT', num)]
+                            num = (block[i][1] // block[i + 1][1]) % MAX
+                            block[i:i + 3] = [('INT', num, block[i + 1][2])]
                             changed = True
-                        if changed:
-                            break
+                        elif block[i + 2][0] == 'STORE':
+                            block[i:i + 3] = [('STORE',
+                                               (block[i][1], block[i + 1][1]),
+                                               block[i + 1][2])]
+                    if changed:
+                        break
                 except IndexError:
                     if changed:
                         break
-            if changed:
-                break
     return ast
 
 
@@ -85,12 +134,19 @@ def main():
     with open(args.file) as file:
         code = file.read()
     ast = parse(code)
+    print(ast)
+    print('Parsed')
     ast = optimize(ast)
+    print('Optimized')
+    print(ast)
     if args.target == 'python':
-        output = to_python.to_python(ast)
+        output = to_python.to_python(ast, MAX=MAX)
+    elif args.target == 'js':
+        output = to_javascript.to_javascript(ast, MAX=MAX)
     else:
         print('Unknown language')
-    print(output)
+        return
+    print('Compiled')
     with open(args.output, 'w') as file:
         file.write(output)
 
--- to_python.py
+++ to_python.py
@@ -1,5 +1,4 @@
-MAX = 18446744073709551615
-def to_python(ast):
+def to_python(ast, MAX):
     code = '''
 from array import array
 import sys
@@ -41,62 +40,203 @@ while True:
     '''
     for name in ast:
         code += 'if name == %s:\n' % name
-        for op_type, op in ast[name]:
+        for op_type, op, stack_min in ast[name]:
+            code += '\n # (%s, %s, %s)\n' % (op_type, op, stack_min) 
             if op_type == 'INT':
-                code += '        stack.append(%s)\n' % op
+                code += '{t}stack.append(%s)\n' % op
             elif op_type == 'ADD':
-                code += '        if len(stack) > 1:\n'
-                code += '            now = (stack[-2] + stack[-1]) % {MAX}\n'
-                code += '            del stack[-2:]\n'
-                code += '            stack.append(now)\n'
-                code += '        elif stack:\n'
-                code += '            stack[-1] = (stack[-1] + 17) % {MAX}\n'
-                code += '        else:\n'
-                code += '            stack.append(34)\n'
+                if isinstance(op, int):
+                    if stack_min:
+                        code += '{t}stack[-1] += %s\n' % op
+                        code += '{t}stack[-1] %= {MAX}\n'
+                    else:
+                        code += '{t}if stack:\n'
+                        code += '{t}    stack[-1] += %s\n' % op
+                        code += '{t}    stack[-1] %= {MAX}\n'
+                        code += '{t}else:\n'
+                        code += '{t}    stack.append(%s)\n' % ((17 + op) % MAX)
+                else:
+                    if stack_min >= 2:
+                        code += '{t}now = (stack[-2] + stack[-1]) % {MAX}\n'
+                        code += '{t}del stack[-2:]\n'
+                        code += '{t}stack.append(now)\n'
+                    elif stack_min:
+                        code += '{t}if len(stack) >= 2:\n'
+                        code += '{t}    now = (stack[-2] + stack[-1]) % {MAX}\n'
+                        code += '{t}    del stack[-2:]\n'
+                        code += '{t}    stack.append(now)\n'
+                        code += '{t}else:\n'
+                        code += '{t}    stack[-1] = (17 + stack[-1]) % {MAX}\n'
+                    else:
+                        code += '{t}if len(stack) >= 2:\n'
+                        code += '{t}    now = (stack[-2] + stack[-1]) % {MAX}\n'
+                        code += '{t}    del stack[-2:]\n'
+                        code += '{t}    stack.append(now)\n'
+                        code += '{t}elif stack:\n'
+                        code += '{t}    stack[-1] = (17 + stack[-1]) % {MAX}\n'
+                        code += '{t}else:\n'
+                        code += '{t}    stack.append(34)\n'
             elif op_type == 'SUB':
-                code += '        if len(stack) > 1:\n'
-                code += '            now = (stack[-2] - stack[-1]) % {MAX}\n'
-                code += '            del stack[-2:]\n'
-                code += '            stack.append(now)\n'
-                code += '        elif stack:\n'
-                code += '            stack[-1] = (stack[-1] - 17) % {MAX}\n'
-                code += '        else:\n'
-                code += '            stack.append(0)\n'
+                if isinstance(op, int):
+                    if stack_min:
+                        code += '{t}stack[-1] -= %s\n' % op
+                        code += '{t}stack[-1] %= {MAX}\n'
+                    else:
+                        code += '{t}if stack:\n'
+                        code += '{t}    stack[-1] -= %s\n' % op
+                        code += '{t}    stack[-1] %= {MAX}\n'
+                        code += '{t}else:\n'
+                        code += '{t}    stack.append(%s)\n' % ((17 - op) % MAX)
+                else:
+                    if stack_min >= 2:
+                        code += '{t}now = (stack[-2] - stack[-1]) % {MAX}\n'
+                        code += '{t}del stack[-2:]\n'
+                        code += '{t}stack.append(now)\n'
+                    elif stack_min:
+                        code += '{t}if len(stack) >= 2:\n'
+                        code += '{t}    now = (stack[-2] - stack[-1]) % {MAX}\n'
+                        code += '{t}    del stack[-2:]\n'
+                        code += '{t}    stack.append(now)\n'
+                        code += '{t}else:\n'
+                        code += '{t}    stack[-1] = (17 - stack[-1]) % {MAX}\n'
+                    else:
+                        code += '{t}if len(stack) >= 2:\n'
+                        code += '{t}    now = (stack[-2] - stack[-1]) % {MAX}\n'
+                        code += '{t}    del stack[-2:]\n'
+                        code += '{t}    stack.append(now)\n'
+                        code += '{t}elif stack:\n'
+                        code += '{t}    stack[-1] = (17 - stack[-1]) % {MAX}\n'
+                        code += '{t}else:\n'
+                        code += '{t}    stack.append(0)\n'
             elif op_type == 'MUL':
-                code += '        if len(stack) > 1:\n'
-                code += '            now = (stack[-2] * stack[-1]) % {MAX}\n'
-                code += '            del stack[-2:]\n'
-                code += '            stack.append(now)\n'
-                code += '        elif stack:\n'
-                code += '            stack[-1] = (stack[-1] * 17) % {MAX}\n'
-                code += '        else:\n'
-                code += '            stack.append(%s)\n' % (17 ** 2)
+                if isinstance(op, int):
+                    if stack_min:
+                        code += '{t}stack[-1] *= %s\n' % op
+                        code += '{t}stack[-1] %= {MAX}\n'
+                    else:
+                        code += '{t}if stack:\n'
+                        code += '{t}    stack[-1] *= %s\n' % op
+                        code += '{t}    stack[-1] %= {MAX}\n'
+                        code += '{t}else:\n'
+                        code += '{t}    stack.append(%s)\n' % ((17 * op) % MAX)
+                else:
+                    if stack_min >= 2:
+                        code += '{t}now = (stack[-2] * stack[-1]) % {MAX}\n'
+                        code += '{t}del stack[-2:]\n'
+                        code += '{t}stack.append(now)\n'
+                    elif stack_min:
+                        code += '{t}if len(stack) >= 2:\n'
+                        code += '{t}    now = (stack[-2] * stack[-1]) % {MAX}\n'
+                        code += '{t}    del stack[-2:]\n'
+                        code += '{t}    stack.append(now)\n'
+                        code += '{t}else:\n'
+                        code += '{t}    stack[-1] = (17 * stack[-1]) % {MAX}\n'
+                    else:
+                        code += '{t}if len(stack) >= 2:\n'
+                        code += '{t}    now = (stack[-2] * stack[-1]) % {MAX}\n'
+                        code += '{t}    del stack[-2:]\n'
+                        code += '{t}    stack.append(now)\n'
+                        code += '{t}elif stack:\n'
+                        code += '{t}    stack[-1] = (17 * stack[-1]) % {MAX}\n'
+                        code += '{t}else:\n'
+                        code += '{t}    stack.append(%s)\n' % (17 * 17)
             elif op_type == 'DIV':
-                code += '        if len(stack) > 1:\n'
-                code += '            now = int(stack[-2] / stack[-1]) % {MAX}\n'
-                code += '            del stack[-2:]\n'
-                code += '            stack.append(now)\n'
-                code += '        elif stack:\n'
-                code += '            stack[-1] = int(stack[-1] / 17) % {MAX}\n'
-                code += '        else:\n'
-                code += '            stack.append(1)\n'
+                if isinstance(op, int):
+                    if stack_min:
+                        if op == 0:
+                            code += '{t}stack[-1] = 17\n'
+                        else:
+                            code += '{t}stack[-1] //= %s\n' % op
+                    else:
+                        code += '{t}if stack:\n'
+                        if op == 0:
+                            code += '{t}    stack[-1] = 17\n' % op
+                        else:
+                            code += '{t}    stack[-1] //= %s\n' % op
+                        code += '{t}else:\n'
+                        if op == 0:
+                            code += '{t}    stack.append(17)\n'
+                        else:
+                            code += '{t}    stack.append(%s)\n' % (17 // op)
+                else:
+                    if stack_min >= 2:
+                        code += '{t}try:\n'
+                        code += '{t}    {t}now = (stack[-2] // stack[-1])\n'
+                        code += '{t}except ZeroDivisionError:\n'
+                        code += '{t}    now = 17\n'
+                        code += '{t}del stack[-2:]\n'
+                        code += '{t}stack.append(now)\n'
+                    elif stack_min:
+                        code += '{t}if len(stack) >= 2:\n'
+                        code += '{t}    try:\n'
+                        code += '{t}        now = (stack[-2] // stack[-1])\n'
+                        code += '{t}    except ZeroDivisionError:\n'
+                        code += '{t}        now = 17\n'
+                        code += '{t}    del stack[-2:]\n'
+                        code += '{t}    stack.append(now)\n'
+                        code += '{t}else:\n'
+                        code += '{t}    try:\n'
+                        code += '{t}        stack[-1] = (17 // stack[-1])\n'
+                        code += '{t}    except ZeroDivisionError:\n'
+                        code += '{t}        stack[-1] = 17\n'
+                    else:
+                        code += '{t}if len(stack) >= 2:\n'
+                        code += '{t}    try:\n'
+                        code += '{t}        now = (stack[-2] // stack[-1])\n'
+                        code += '{t}    except ZeroDivisionError:\n'
+                        code += '{t}        now = 17\n'
+                        code += '{t}    del stack[-2:]\n'
+                        code += '{t}    stack.append(now)\n'
+                        code += '{t}elif stack:\n'
+                        code += '{t}    try:\n'
+                        code += '{t}        stack[-1] = (17 // stack[-1])\n'
+                        code += '{t}    except ZeroDivisionError:\n'
+                        code += '{t}        stack[-1] = 17\n'
+                        code += '{t}else:\n'
+                        code += '{t}    stack.append(17)\n'
             elif op_type == 'STORE':
-                code += '        if len(stack) > 1:\n'
-                code += '            mem.set(stack[-1], stack[-2])\n'
-                code += '            del stack[-2:]\n'
-                code += '        elif stack:\n'
-                code += '            mem.set(stack.pop(-1), 17)\n'
-                code += '        else:\n'
-                code += '            mem.set(17, 17)\n'
+                if isinstance(op, tuple):
+                    code += '{t}mem.set(%s, %s)\n' % (op[1], op[0])
+                elif isinstance(op, int):
+                    code += '{t}mem.set(%s, stack[-1])\n' % op
+                    code += '{t}del stack[-1]\n'
+                else:
+                    code += '{t}mem.set(stack[-1], stack[-2])\n'
+                    code += '{t}del stack[-2:]\n'
             elif op_type == 'LOAD':
-                code += '        if stack:\n'
-                code += '            stack.append(mem.get(stack.pop(-1)))\n'
-                code += '        else:\n'
-                code += '            stack.append(mem.get(17))\n'
-            
+                if isinstance(op, int):
+                    code += '{t}stack.append(mem.get(%s))\n' % op
+                else:
+                    code += '{t}stack.append(mem.get(stack.pop(-1)))\n'
+            elif op_type == 'DUP':
+                if isinstance(op, int):
+                    code += '{t}stack.append(%s)' % op
+                else:
+                    code += '{t}stack.append(stack[-1])'
+            elif op_type == 'EQ':
+                code += '{t}now = 1 if stack[-2] == stack[-1] else 0\n'
+                code += '{t}del stack[-2:]\n'
+                code += '{t}stack.append(now)\n'
+            elif op_type == 'NTEQ':
+                code += '{t}now = 1 if stack[-2] != stack[-1] else 0\n'
+                code += '{t}del stack[-2:]\n'
+                code += '{t}stack.append(now)\n'
+            elif op_type == 'GREATER':
+                code += '{t}now = 1 if stack[-2] > stack[-1] else 0\n'
+                code += '{t}del stack[-2:]\n'
+                code += '{t}stack.append(now)\n'
+            elif op_type == 'LESS':
+                code += '{t}now = 1 if stack[-2] < stack[-1] else 0\n'
+                code += '{t}del stack[-2:]\n'
+                code += '{t stack.append(now)\n'
             elif op_type == 'OUTPUT':
-                code += '        print(stack.pop(-1))\n'
-            print(op_type, op)
+                if isinstance(op, int):
+                    code += '{t}print(%s)\n' % op
+                else:
+                    code += '{t}print(stack.pop(-1))\n'
+            else:
+                print('Unknown op_type:', op_type)
+            print(op_type, op, stack_min)
         code += '    el'
     code += 'se:\n        break'  
-    return code.format(MAX=MAX)
+    return code.format(MAX=MAX, t=' ' * 8)
